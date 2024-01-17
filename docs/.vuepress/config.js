824639e4bf5c644e7cab66265234fc6c73948e98
dengc4r <zero.dengc@outlook.com>
2024-01-17 10:33:41 +0800

    feat:删除node_modules

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,23 +1,30 @@
-import {defaultTheme} from 'vuepress'
+import { defaultTheme } from "vuepress";
 
 export default {
-    title: 'SmartDengC',
-    theme: defaultTheme({
-
-        logo: "/assets/img/hero.png",
-        sidebar: {
-            '/guide/':[
-                {
-                    text: 'Guide',
-                    collapsible: true,
-                    children: ['/guide/README.md', '/guide/2024-01-16-GIT经历0.md', '/guide/StompJS监听RabbitMq.md']
-                }
-            ]
+  title: "阿聪的小破站",
+  head: [
+    ["link", { rel: "icon", href: "/assets/img/logo.png" }],
+    ["meta", { name: "author", content: "SmartDengC" }],
+  ],
+  theme: defaultTheme({
+    logo: "/assets/img/hero.png",
+    sidebar: {
+      "/guide/": [
+        {
+          text: "Guide",
+          collapsible: true,
+          children: [
+            "/guide/README.md",
+            "/guide/2024-01-16-GIT经历0.md",
+            "/guide/StompJS监听RabbitMq.md",
+          ],
         },
-        navbar:[
-            {text:'首页', link: '/'},
-            {text:'ABOUT', link: '/about'},
-            {text: 'GUIDE', link: '/guide'},
-        ]
-    })
+      ],
+    },
+    navbar: [
+      { text: "首页", link: "/" },
+      { text: "ABOUT", link: "/about" },
+      { text: "GUIDE", link: "/guide" },
+    ],
+  }),
 };
