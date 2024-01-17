2e6be5a82de03ebb4091fa22212016b41a2dbb62
dengc4r <zero.dengc@outlook.com>
2024-01-17 13:04:04 +0800

    feat:添加部分插件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,10 @@
+import googleAnalyticsPlugin from "@vuepress/plugin-google-analytics";
 import { defaultTheme } from "vuepress";
+import themeSidebar from "vuepress-theme-sidebar";
+import { searchPlugin } from "@vuepress/plugin-search";
+
+// const autoSidebar = require("vuepress-plugin-auto-sidebar");
+// 侧边栏的插件使用不了 https://github.com/shanyuhai123/vuepress-plugin-auto-sidebar
 
 export default {
   title: "阿聪的小破站",
@@ -7,24 +13,31 @@ export default {
     ["meta", { name: "author", content: "SmartDengC" }],
   ],
   theme: defaultTheme({
+    // theme: themeSidebar({
     logo: "/assets/img/hero.png",
-    sidebar: {
-      "/guide/": [
-        {
-          text: "Guide",
-          collapsible: true,
-          children: [
-            "/guide/README.md",
-            "/guide/2024-01-16-GIT经历0.md",
-            "/guide/StompJS监听RabbitMq.md",
-          ],
-        },
-      ],
-    },
+    // sidebar: {
+    //   "/guide/": [
+    //     {
+    //       text: "Guide",
+    //       collapsible: true,
+    //       children: [
+    //         "/guide/README.md",
+    //         "/guide/2024-01-16-GIT经历0.md",
+    //         "/guide/StompJS监听RabbitMq.md",
+    //       ],
+    //     },
+    //   ],
+    // },
     navbar: [
       { text: "首页", link: "/" },
       { text: "ABOUT", link: "/about" },
       { text: "GUIDE", link: "/guide" },
     ],
   }),
+  plugins: [
+    searchPlugin({}),
+    googleAnalyticsPlugin({
+      id: "G-XXXXXXXXXX",
+    }),
+  ],
 };
