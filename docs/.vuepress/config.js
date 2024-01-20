8caa206288c189c70c30b64bb9b58ef744f224d8
dengc4r <zero.dengc@outlook.com>
2024-01-20 22:27:55 +0800

    feat:优化v0.2

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,10 +11,9 @@ export default {
     // base: "/docs/",  
   title: "阿聪的小破站",
   head: [
-    ["link", { rel: "icon", href: "/assets/img/logo.png" }],
+    ["link", { rel: "icon", href: "/assets/img/F.png" }],
     ["meta", { name: "author", content: "SmartDengC" }],
     ["meta", { name: "referrer", content: "no-referrer" }],  // 处理md文件中不展示gitee图片
-
   ],
   // theme: defaultTheme({
   theme: themeSidebar({
--- docs/vuepress2.0/README.md
+++ docs/vuepress2.0/README.md
@@ -1,1 +1,6 @@
+---
+title: readme.md
+---
+
+
 总结vuepress2.0学习过程，包括但不限于问题。
