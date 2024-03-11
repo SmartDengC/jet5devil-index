00f9dad5348b671b60b52314dc8c679cfd9ed1b7
dengc4r <zero.dengc@outlook.com>
2024-03-11 19:13:17 +0800

    fix(config.js): 修改导航栏信息

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -71,51 +71,62 @@ export default defineUserConfig({
     // },
     navbar: [
       { text: "首页", link: "/", icon: "material-symbols:home" },
-      { text: "博客", link: "/blog/", icon: "fluent-mdl2:reading-mode-solid" },
       { text: "博客标签", link: "/blog/tags/", icon: "solar:tag-bold" },
-      {
-        text: "关于",
-        icon: "mdi:about",
-        // link: "/about/",
-        items: [
-          { text: "关于自己", link: "/about", icon: "cib:player-me" },
-          { text: "博客标签", link: "/blog/tags/", icon: "solar:tag-bold" },
-          {
-            text: "博客归档",
-            link: "/blog/archives/",
-            icon: "mingcute:triumphal-arch-fill",
-          },
-          {
-            text: "友情链接",
-            link: "/friends/",
-            icon: "fa-solid:user-friends",
-          },
-        ],
-      },
+      { text: "博客", link: "/blog/", icon: "fluent-mdl2:reading-mode-solid" },
+      // {
+      //   text: "关于",
+      //   icon: "mdi:about",
+      //   // link: "/about/",
+      //   items: [
+      //     { text: "关于自己", link: "/about", icon: "cib:player-me" },
+      //     // { text: "博客标签", link: "/blog/tags/", icon: "solar:tag-bold" },
+      //     // {
+      //     //   text: "博客归档",
+      //     //   link: "/blog/archives/",
+      //     //   icon: "mingcute:triumphal-arch-fill",
+      //     // },
+      //     {
+      //       text: "友情链接",
+      //       link: "/friends/",
+      //       icon: "fa-solid:user-friends",
+      //     },
+      //   ],
+      // },
       // {
       //   text: "归档",
       //   link: "/blog/archives/",
       //   icon: "mingcute:triumphal-arch-fill",
       // },
-      // {
-      //   text: "Vuepress2.0",
-      //   link: "/frontend/vuepress2.0",
-      //   icon: "devicon-plain:vuetify",
-      // },
+      {
+        text: "站点导航",
+        link: "/article/ixu7719i/",
+        icon: "cib:player-me",
+      },
+      {
+        text: "书籍导航",
+        link: "/article/w9boeev6/",
+        icon: "solar:tag-bold",
+      },
       {
         text: "更多",
         icon: "mdi:about",
         // link: "/about",
         items: [
+          { text: "关于自己", link: "/about", icon: "cib:player-me" },
+          // {
+          //   text: "站点导航",
+          //   link: "/article/ixu7719i/",
+          //   icon: "cib:player-me",
+          // },
+          // {
+          //   text: "书籍导航",
+          //   link: "/article/w9boeev6/",
+          //   icon: "solar:tag-bold",
+          // },
           {
-            text: "站点导航",
-            link: "/article/ixu7719i/",
-            icon: "cib:player-me",
-          },
-          {
-            text: "书籍导航",
-            link: "/article/w9boeev6/",
-            icon: "solar:tag-bold",
+            text: "友情链接",
+            link: "/friends/",
+            icon: "fa-solid:user-friends",
           },
         ],
       },
--- docs/about/webNavbar.md
+++ docs/about/webNavbar.md
@@ -7,15 +7,19 @@ permalink: /article/ixu7719i/
 
 收集各种网站，方便导航
 
-## vuepress
+## Software of Mac
+
+强势记录两个好用的软件地址， 欢迎体验 👏🏻
+
+[macked.app](https://macked.app/)
+
+[macwk.cn](https://macwk.cn/)
+
+## Vuepress
 
 Emoji 支持 [ikatyang/emoji-cheat-sheet](https://github.com/ikatyang/emoji-cheat-sheet)  
 Icon 支持 [icon-sets.iconify.design](https://icon-sets.iconify.design/)
 
-
-
 ## WebSite
 
-[MacVK](https://macwk.cn/)
-
-[ITELLYOU](https://next.itellyou.cn/)
+[next.itellyou.cn](https://next.itellyou.cn/) 新版 OS 聚集地
