72f07870f21c017d88f9a46c5a7b856e47d8a649
dengc4r <zero.dengc@outlook.com>
2025-07-17 00:19:19 +0800

    feat(wiki): java se

--- wiki/.vuepress/notes/interview.ts
+++ wiki/.vuepress/notes/interview.ts
@@ -8,7 +8,16 @@ export default defineNoteConfig({
       text: "", // 第一层如果为空，自动折叠需要设置为false
       icon: "solar:tag-bold",
       collapsed: false,
-      items: ["0.介绍"],
+      items: [
+        "0.介绍",
+
+        {
+          text: "二哥的Java进阶之路", // 第一层如果为空，自动折叠需要设置为false
+          icon: "solar:tag-bold",
+          collapsed: false,
+          items: ["二哥的Java进阶之路/面渣逆袭-Java SE"],
+        },
+      ],
     },
   ],
 });
---
+++ wiki/notes/Interview/二哥的Java进阶之路/面渣逆袭-Java SE.md
@@ -0,0 +1,15 @@
+---
+title: 面渣逆袭-Java SE
+createTime: 2025/07/16 23:10:28
+permalink: /interview/xkyfo3ia/
+---
+
+1、什么是Java？
+
+2、Java语言有哪些特点？
+
+3、JVM、JDK和JRE有什么区别？
+
+4、说说什么是跨平台？原理是什么？
+
+5、什么是字节码？采用字节码的好处是什么？
