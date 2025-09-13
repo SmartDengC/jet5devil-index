c983bf712c653ad215a25bfae928476a269bdc7c
dengc4r <zero.dengc@outlook.com>
2025-09-13 21:40:13 +0800

    feat(theme): 调整主题

--- vuepress.client.ts
+++ vuepress.client.ts
@@ -2,8 +2,6 @@ import { defineClientConfig } from "vuepress/client";
 import NpmBadge from "vuepress-theme-plume/features/NpmBadge.vue";
 import NpmBadgeGroup from "vuepress-theme-plume/features/NpmBadgeGroup.vue";
 import RepoCard from "vuepress-theme-plume/features/RepoCard.vue";
-import Landing from "./wiki/.vuepress/theme/components/Landing.vue";
-import { useStatistics } from "./wiki/.vuepress/theme/composables/statistics";
 
 import "./wiki/.vuepress/theme/styles/index.css";
 import ColourPicker from "./_source/components/ColourPicker.vue";
@@ -15,11 +13,7 @@ export default defineClientConfig({
     app.component("RepoCard", RepoCard);
     app.component("NpmBadge", NpmBadge);
     app.component("NpmBadgeGroup", NpmBadgeGroup);
-    app.component("Landing", Landing);
     app.component("ColourPicker", ColourPicker);
-  },
-  setup() {
-    useStatistics();
   },
 
   // inject custom layout components
