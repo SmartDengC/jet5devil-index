53e712029f3a542ab6f88a733f581a90ed299cfc
smartdengc <zero.dengc@outlook.com>
2025-12-29 19:31:43 +0800

    feat(blog): 更新2025田协10公里精英赛和重庆马拉松内容

--- sci/.vuepress/collections/mood.ts
+++ sci/.vuepress/collections/mood.ts
@@ -9,7 +9,11 @@ export default defineCollection({
     {
       text: "随笔",
       icon: "tabler:mood-tongue-wink",
-      items: ["给一年后自己的一封信", "给四年后自己的一封信"],
+      items: [
+        "给一年后自己的一封信",
+        "给四年后自己的一封信",
+        "给现在迷茫的自己",
+      ],
     },
   ],
 });
